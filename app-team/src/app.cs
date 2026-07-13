045cae8ea375836943852290e6ad671324ccc03a
Sebastián Aguado <1451419+saguadob@users.noreply.github.com>
2026-07-13 16:33:59 +0000

    Added files for small app compilation

--- app-team/src/app.cs
+++ app-team/src/app.cs
@@ -3,40 +3,44 @@
 #:sdk Microsoft.NET.Sdk.Web
 #:package Npgsql.DependencyInjection@10.0.3
 #:package Dapper@2.1.72
+#:package Dapper.AOT@1.0.52
+#:property InterceptorsPreviewNamespaces=$(InterceptorsPreviewNamespaces);Dapper.AOT
 
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.Sqlite;
 using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+[module: DapperAot]
 
 var appBuilder = WebApplication.CreateBuilder(args);
 
 #region Configure web application
 appBuilder.Logging.AddConsole();
 appBuilder.Configuration.AddEnvironmentVariables();
-builder.Services.AddNpgsqlDataSource(/* Uses Environment Variables as documented here https://www.npgsql.org/doc/connection-string-parameters.html#environment-variables */);
+appBuilder.Services.AddNpgsqlSlimDataSource(appBuilder.Configuration.GetConnectionString("DefaultConnection")!);
 #endregion
 
 var app = appBuilder.Build();
 
-async Task<IResult> GetLink([FromServices] IDbConnection db, string id)
+async Task<IResult> GetLink([FromServices] NpgsqlConnection db, string id)
 {
 	var rawUrlString = await db.ExecuteScalarAsync<string>($"SELECT url FROM Links WHERE id = @Id", new { Id = id});
-	return link is not null ? TypedResults.Redirect(link, permanent: true) : TypedResults.NotFound();
+	return rawUrlString is not null ? TypedResults.Redirect(rawUrlString, permanent: true) : TypedResults.NotFound();
 }
 app.MapGet("/{id}", GetLink).WithName(nameof(GetLink));
 app.Logger.LogInformation("Configured link retrieval");
 
-async Task<IResult> Create([FromServices] IDbConnection db, string? id, [FromForm] string url)
+async Task<IResult> Create([FromServices] NpgsqlConnection db, string? id, [FromForm] string url)
 {
-		var linkId = id is null ? Path.GetFileNameWithoutExtension(Path.GetRandomFileName() : id;
+		var linkId = id is null ? Path.GetFileNameWithoutExtension(Path.GetRandomFileName()): id;
     if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
     {
-				var rowsAffected = await _db.ExecuteAsync($"INSERT INTO ShortUrl (id, url) VALUES (@Id, @Url)", new { Id = id, Url = url });
-        return TypedResults.CreatedAtRoute(nameof(GetLink), new {id});
+				var rowsAffected = await db.ExecuteAsync($"INSERT INTO Links (id, url) VALUES (@Id, @Url)", new { Id = linkId, Url = url });
+        return Results.CreatedAtRoute(nameof(GetLink), new RouteValueDictionary { ["id"] = linkId });
     }
     else
     {
-        return TypedResults.BadRequest("Invalid or missing URL");
+        return Results.BadRequest("Invalid or missing URL");
     }
 }
 app.MapPost("/link", Create).DisableAntiforgery().WithName("CreateRandom");
---
+++ app-team/src/linkerdb.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Links (
+		id char(20) PRIMARY KEY,
+		url text
+);
